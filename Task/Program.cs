92daed13c6053044a9d591b1bdce334c178db64b
Nazar Bulyha <nazikkun97@gmail.com>
2019-04-10 05:38:47 +0300

    Finish task.

--- Task/Program.cs
+++ Task/Program.cs
@@ -1,10 +1,25 @@
 ﻿namespace Task
 {
+    using System;
+
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            string sampleText = "aaaabbbcccccaaddddddcfggghhhh";
 
+            var resultSplit = StringHelper.SplitStringByChars(sampleText);
+            var charOrChars = resultSplit.Item2.Count == 1 ? "Char" : "Chars";
+
+            Console.WriteLine($"{charOrChars} count in string: {resultSplit.Item1}");
+            Console.Write($"{charOrChars}: ");
+
+            foreach (var symbols in resultSplit.Item2)
+            {
+                Console.Write(symbols + " ");
+            }
+
+            Console.ReadKey();
         }
     }
 }
---
+++ Task/StringHelper.cs
@@ -0,0 +1,39 @@
+﻿namespace Task
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public static class StringHelper
+    {
+        public static (int, IList<char>) SplitStringByChars(string text)
+        {
+            if (string.IsNullOrWhiteSpace(text))
+            {
+                throw new ArgumentException("Text can not be empty or null.");
+            }
+
+            /*  Also we can group our string by chars with regex pattern:
+                
+                var regex = new Regex("(?<=(.))(?!\\1|$)");
+                var groupedMembers = regex.Split(text);
+            */
+
+            // group from smth like xyxzyz => xxyyzz
+            var groupedMembers = text.GroupBy(x => x).Select(group => new {
+                CharValue = group.Key,
+                CharCount = group.Count()
+            }).OrderByDescending(x => x.CharCount);
+
+            // get longest char chain in string
+            var maxCharCount = groupedMembers.First().CharCount;
+
+            // also check if there are another char with the same longest chain
+            var resultChars = groupedMembers.Where(x => x.CharCount == maxCharCount)
+                .Select(x => x.CharValue)
+                .ToList();
+
+            return (maxCharCount, resultChars);
+        }
+    }
+}
