bf69cf358d71201d36219e514f7f4762857a683d
Nazar Bulyha <nazikkun97@gmail.com>
2019-04-10 05:59:51 +0300

    Add unit tests to project (pass them).

---
+++ UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3988d671-0898-443c-881e-8669e3c27b6e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnitTest/UnitTest1.cs
@@ -0,0 +1,96 @@
+﻿namespace TaskTest
+{
+    using System;
+    using System.Linq;
+    using System.Collections.Generic;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Task;
+
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void Test_Output_Should_Return_One_Element()
+        {
+            // Arrange
+            var text = "a";
+
+            // Act
+            var expected = StringHelper.SplitStringByChars(text);
+
+            // Asset
+            var countChars = 1;
+            var actual = new List<char> { 'a' };
+
+            Assert.AreEqual(countChars, expected.Item1);
+            Assert.IsTrue(expected.Item2.SequenceEqual(actual));
+        }
+
+        [TestMethod]
+        public void Test_Output_Should_Return_One_Element1()
+        {
+            // Arrange
+            var text = "ababcca";
+
+            // Act
+            var expected = StringHelper.SplitStringByChars(text);
+
+            // Asset
+            var countChars = 3;
+            var actual = new List<char> { 'a' };
+
+            Assert.AreEqual(countChars, expected.Item1);
+            Assert.IsTrue(expected.Item2.SequenceEqual(actual));
+        }
+
+        [TestMethod]
+        public void Test_Output_Should_Return_Two_Elements()
+        {
+            // Arrange
+            var text = "aaabbbc";
+
+            // Act
+            var expected = StringHelper.SplitStringByChars(text);
+
+            // Asset
+            var countChars = 3;
+            var actual = new List<char> { 'a', 'b' };
+
+            Assert.AreEqual(countChars, expected.Item1);
+            Assert.IsTrue(expected.Item2.SequenceEqual(actual));
+        }
+
+        [TestMethod]
+        public void Test_Output_Should_Return_Three_Elements()
+        {
+            // Arrange
+            var text = "abc";
+
+            // Act
+            var expected = StringHelper.SplitStringByChars(text);
+
+            // Asset
+            var countChars = 1;
+            var actual = new List<char> { 'a', 'b', 'c' };
+
+            Assert.AreEqual(countChars, expected.Item1);
+            Assert.IsTrue(expected.Item2.SequenceEqual(actual));
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Text can not be empty or null.")]
+        public void Empty_String_Should_Return_Exception()
+        {
+            var emptyString = "";
+            var result = StringHelper.SplitStringByChars(emptyString);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), "Text can not be empty or null.")]
+        public void Whitespace_String_Should_Return_Exception()
+        {
+            var whitespaceString = "   ";
+            var result = StringHelper.SplitStringByChars(whitespaceString);
+        }
+    }
+}
